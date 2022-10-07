45939f116c1900daecd53e77b73d5559b354a6f3
Hamachi <hamachi@Ivans-MacBook-Pro.local>
2022-10-07 23:56:28 +0800

    Added some safety checks to prevent people from accidentally clicking button

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -10,7 +10,9 @@ const Home: NextPage = () => {
   const [a, setA] = useState("");
   const [b, setB] = useState("")
   const {
-    contractInterface
+    contractInterface,
+    isConnected
+
   } = useWalletContext()
 
 
@@ -40,36 +42,39 @@ const Home: NextPage = () => {
       <main className="container mx-auto flex min-h-screen flex-col items-center justify-center p-4">
 
         <WalletButton />
-        <p>Your last calculation was {result}</p>
-        <div className="flex flex-col">
-          <h1>Calculator</h1>
-          <form>
-            <div>
-              <label>First Value</label>
-              <input
-                onChange={(e) => setA(e.target.value)}
-                value={a}
-                className="border mx-4" type="text" />
-            </div>
-            <div>
-              <label>Second Value</label>
-              <input
-                onChange={(e) => setB(e.target.value)}
-                className="border mx-4" type="text" value={b} />
-            </div>
+        {!isConnected && <p>Please connect your wallet to see the rest of the app</p>}
 
-            <button
-              onClick={(e) => onAdd(e)}
-              className="border">
-              Add
-            </button>
-            <button
-              onClick={(e) => onMinus(e)}
-              className="border">
-              Minus
-            </button>
-          </form>
-        </div>
+        {isConnected &&
+          <><p>Your last calculation was {result}</p>
+            <div className="flex flex-col">
+              <h1>Calculator</h1>
+              <form>
+                <div>
+                  <label>First Value</label>
+                  <input
+                    onChange={(e) => setA(e.target.value)}
+                    value={a}
+                    className="border mx-4" type="text" />
+                </div>
+                <div>
+                  <label>Second Value</label>
+                  <input
+                    onChange={(e) => setB(e.target.value)}
+                    className="border mx-4" type="text" value={b} />
+                </div>
+
+                <button
+                  onClick={(e) => onAdd(e)}
+                  className="border">
+                  Add
+                </button>
+                <button
+                  onClick={(e) => onMinus(e)}
+                  className="border">
+                  Minus
+                </button>
+              </form></div></>}
+
       </main>
     </>
   );
